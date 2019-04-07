6a7c55e225b0abc67bbceacdacc8a844292a837d
wdx9652 <1761814150@qq.com>
2019-04-07 15:05:34 +0800

    组长在次更新了Main方法

--- src/main/java/com/qf/main/Student.java
+++ src/main/java/com/qf/main/Student.java
@@ -1,6 +1,10 @@
 package com.qf.main;
 
 public class Student {
+    private Integer id;
+    private String name;
+
+
     public static void main(String[] args) {
         System.out.println("我是组员一提交的代码一");
     }
