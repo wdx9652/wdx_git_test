9489755dfdebcccaa3298ed2ec8559cee750924d
wdx9652 <1761814150@qq.com>
2019-04-07 15:03:57 +0800

    组长更新了Main方法

--- src/main/java/com/qf/main/Main.java
+++ src/main/java/com/qf/main/Main.java
@@ -2,6 +2,6 @@ package com.qf.main;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("今与2018年4月7日");
+        System.out.println("韦德祥今与2018年4月7日");
     }
 }
